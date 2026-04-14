b5f381fb39175c748d4ca7e9c5c638f8ead64575
GeorgesKaplan <dimitri.nantet@gmail.com>
2026-04-14 15:57:40 +0200

    Secured Weapon.hhp inclusion

--- ex03/include/Weapon.hpp
+++ ex03/include/Weapon.hpp
@@ -6,12 +6,13 @@
 /*   By: dnantet <dnantet@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/14 13:10:24 by dnantet           #+#    #+#             */
-/*   Updated: 2026/04/14 15:39:24 by dnantet          ###   ########.fr       */
+/*   Updated: 2026/04/14 15:49:46 by dnantet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#pragma once
-#include <iostream>
+#ifndef WEAPON_HPP
+# define WEAPON_HPP
+# include <iostream>
 
 class Weapon
 {
@@ -27,3 +28,5 @@ public:
 	Weapon(void);
 	~Weapon();
 };
+
+#endif
