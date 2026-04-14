f0724f93f401d70ecd105c7f56a7389cc289def5
GeorgesKaplan <dimitri.nantet@gmail.com>
2026-04-14 10:51:12 +0200

    01: copy-pasted essential files from 00

---
+++ ex01/srcs/classes/Zombie.cpp
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Zombie.cpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dnantet <dnantet@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/04/10 13:15:06 by dnantet           #+#    #+#             */
+/*   Updated: 2026/04/14 10:27:10 by dnantet          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Zombie.hpp"
+
+void Zombie::announce()
+{
+	std::cout << this->_name << GRUNT << std::endl;
+}
+
+Zombie::Zombie(std::string name)
+{
+	this->_name = name;
+	std::cout << "Zombie '" << this->_name << "' has risen!" << std::endl;
+}
+
+Zombie::~Zombie()
+{
+	std::cout << "Zombie '" << this->_name << "' has been sent back six feet under." << std::endl;
+}
