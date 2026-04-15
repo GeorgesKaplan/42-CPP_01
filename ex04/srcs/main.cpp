ecdd0c44923c266bb942517655c2b2f1a3b3fb29
GeorgesKaplan <dimitri.nantet@gmail.com>
2026-04-15 13:30:59 +0200

    04: Can open input file and create output file

--- ex04/srcs/main.cpp
+++ ex04/srcs/main.cpp
@@ -6,7 +6,7 @@
 /*   By: dnantet <dnantet@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/15 12:07:10 by dnantet           #+#    #+#             */
-/*   Updated: 2026/04/15 12:25:03 by dnantet          ###   ########.fr       */
+/*   Updated: 2026/04/15 13:30:01 by dnantet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,16 +17,37 @@ int main(int ac, char *av[])
 {
 	if (ac != 4)
 	{
-		std::cout << "Invalid input, should be `./replace <filename> <to_search> <replace_with>" << std::endl;
+		std::cout << "Invalid input, should be `./replace <infile> <to_search> <replace_with>" << std::endl;
 		return (1);
 	}
 
-	std::string filename = av[1];
+	char *infile = av[1];
 	std::string to_search = av[2];
 	std::string replace_with = av[3];
 
 	std::fstream fs;
-	fs.open(filename, std::fstream::in);
+	fs.open(infile, std::fstream::in);
+	if (!fs)
+	{
+		std::cout << "Error: Couldn't open file '" << infile << "'." << std::endl;
+		return (1);
+	}
+
+	std::ofstream ofs;
+	ofs.open(((std::string)infile + ".replace").c_str());
+	if (!fs)
+	{
+		std::cout << "Error: Couldn't create '" << infile << ".replace'." << std::endl;
+		return (1);
+	}
+
+
+	// std::string line;
+	// while (getline(fs, line))
+	// {
+	// 	std::cout << line << std::endl;
+	// }
+
 
 	fs.close();
 	return (0);
